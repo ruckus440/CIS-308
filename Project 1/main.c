c14908e34755e20902afb3d17405e9ce63e3f986
Mike Ruckert <mwr3333@ksu.edu>
2019-09-05 20:57:26 -0500

    Begin Project 1

---
+++ Project 1/main.c
@@ -0,0 +1,19 @@
+/**********************************************
+* Name: Mike Ruckert *
+* Date: 9/8/2019 *
+* Assignment: Project 1 - Sequence and Order validation *
+***********************************************
+* (WRITE A DESCRIPTION OF THE PROGRAM) *
+***********************************************/
+/**********************************************
+* Description: *
+* Input: *
+* Output: *
+***********************************************/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
