c0ff383ea9bf87dd4e41b0257f7a968286c4b8ea
Mike Ruckert <mwr3333@ksu.edu>
2019-09-11 02:30:36 -0500

    stuck

--- Project1/main.c
+++ Project1/main.c
@@ -16,20 +16,76 @@
 #include <string.h>
 
 int valid = 1;
-int validateSequence (char sequence[]);
-int isOpenParenthesis (char);
-int isClosingParenthesis (char);
+char openParentheses[35];
+int validateSequence (char[]);
+char sequence[35];
+int checkPos;
 
 
-int validateSequence (char sequence[]) {
+int validateSequence (char seq[]) {
 
-    int checkPos = 1;
+    checkPos = 0;
     //alogrithm goes here
-    for (int i = 1; i < strlen(sequence); i++) {
-        if (sequence[i] == ")" || sequence [i] == "]" || sequence[i] == "}") {
-
+    for (int i = 0; i < strlen(seq); i++) {
+        /*
+        if (seq[i] == '(' || seq [i] == '[' || seq[i] == '{') {
+            openParentheses[i] = seq[i];
         }
+        if (seq[i] == ')') {
+            if (openParentheses[i-checkPos] == '('){
+                checkPos--;
+            }
+            else {
+                valid = 0;
+                break;
+            }
+        }
+        if (seq[i] == ']') {
+            if (openParentheses[i-checkPos] == '['){
+                checkPos--;
+            }
+            else {
+                valid = 0;
+                break;
+            }
+        }
+        if (seq[i] == '}') {
+            if (openParentheses[i-checkPos] == '{'){
+                checkPos--;
+            }
+            else {
+                valid = 0;
+                break;
+            }
+        }
+         */
 
+        if (seq[i] == '(' || seq[i] == '[' || seq[i] == '{') {
+            openParentheses[i] = seq[i];
+        }
+        //printf(openParentheses);
+        //printf("\n");
+
+        for (int i = 0; i < strlen(seq); i++) {
+            if (seq[i] == ')') {
+                if (openParentheses[i-1] != '('){
+                    valid = 0;
+                    break;
+                }
+            }
+            if (seq[i] == ']') {
+                if (openParentheses[i-1] != '['){
+                    valid = 0;
+                    break;
+                }
+            }
+            if (seq[i] == '}') {
+                if (openParentheses[i-1] != '{'){
+                    valid = 0;
+                    break;
+                }
+            }
+        }
     }
     return valid;
 }
@@ -39,7 +95,7 @@ int main(int argc, char *argv []) {
     FILE *fp;
     int numOfExpressions = 0;
     char *filename;
-    char data[50];
+    char fileData[50];
     filename = (char *) malloc(sizeof(char) * 15);
 
     strcpy(filename, argv[1]);
@@ -52,11 +108,28 @@ int main(int argc, char *argv []) {
     printf("There are %d lines in this document: \n", numOfExpressions);
 
     if (fp != NULL) {
-        while (fgets(data, 35, fp) != NULL) {
-            printf("%s", data);
+
+        for (int i = 0; i < numOfExpressions; i++) {
+            fscanf(fp, "%s", sequence);
+            printf("%s", sequence);
+            validateSequence(sequence);
+            if (valid) {
+                printf(" - correct\n");
+            } else {
+                printf(" - incorrect\n");
+            }
         }
+        /*
+        while (fgets(fileData, 35, fp) != NULL) {
+            printf("%s", fileData);
+        }
+         */
+
         printf("\n");
+
     }
+
+
 
     printf("File closed.\n");
     fclose(fp);
