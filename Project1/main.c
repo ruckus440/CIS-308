29d6551f3c5aa9c8154f050c15a045d29897b054
Mike Ruckert <mwr3333@ksu.edu>
2019-09-12 12:13:18 -0500

    Project 1 done

--- Project1/main.c
+++ Project1/main.c
@@ -1,137 +1,114 @@
 /**********************************************
 * Name: Mike Ruckert *
-* Date: 9/8/2019 *
+* Date: 9/12/2019 *
 * Assignment: Project 1 - Sequence and Order validation *
 ***********************************************
-* (WRITE A DESCRIPTION OF THE PROGRAM) *
+* This program verifies that an arithmetic sequence of parentheses is balanced.
+ * It reads text line by line from a .txt file.  It gets the number of sequences.
+ * Then it loops that number of times through the file, reading sequences line by line.
+ * If it encounters an open parentheses it adds them to an array.  If it encounters
+ * a close parentheses, it varifies that the last character in the array of open
+ * parentheses matches that particular parentheses.  If so, the open array index is
+ * decremented.  If not, the function exits and the result is printed.*
 ***********************************************/
 /**********************************************
 * Description: *
-* Input: *
-* Output: *
+* Input: A .txt file with an int indicating the number of sequences followed by that number of sequences. *
+* Output: Prints to the console the sequence followed by the the result of validating the sequence. *
 ***********************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int valid = 1;
-char openParentheses[35];
-int validateSequence (char[]);
-char sequence[35];
-int checkPos;
+int valid; //boolean indicator
+int checkExpression(char[]); //function declaration
+char sequence[35]; //stores the sequence read from file
 
+int checkExpression(char input[])
+{
+    char stack[35]; //array to hold open parentheses
+    int stackIndex = 0; //index to keep track of position in stack[]
+    int length = strlen(input); //gets length of the input sequence for loop sentinel
 
-int validateSequence (char seq[]) {
-
-    checkPos = 0;
-    //alogrithm goes here
-    for (int i = 0; i < strlen(seq); i++) {
-        /*
-        if (seq[i] == '(' || seq [i] == '[' || seq[i] == '{') {
-            openParentheses[i] = seq[i];
+    for (int i = 0; i < length; i++) //begin loop validate line from file
+    {
+        if (input[i] == '(' || input[i] == '{' || input[i] == '[') //if reads an open parentheses...
+        {
+            stack[stackIndex] = input[i]; //...add it to stack[]...
+            stackIndex++; //...and increment our index
         }
-        if (seq[i] == ')') {
-            if (openParentheses[i-checkPos] == '('){
-                checkPos--;
+        else if (input[i] == ')') //if read a close parentheses...
+        {
+            if (stack[stackIndex - 1] == '(') //...if the previous parentheses matches...
+            {
+                stackIndex--; //...decrement the index
             }
-            else {
-                valid = 0;
-                break;
-            }
-        }
-        if (seq[i] == ']') {
-            if (openParentheses[i-checkPos] == '['){
-                checkPos--;
-            }
-            else {
-                valid = 0;
-                break;
+            else
+            {
+                printf("Found an unmatched parentheses\n"); //Print the result
+                return 0;
             }
         }
-        if (seq[i] == '}') {
-            if (openParentheses[i-checkPos] == '{'){
-                checkPos--;
+        else if (input[i] == '}') //case for other close parentheses
+        {
+            if (stack[stackIndex - 1] == '{')
+            {
+                stackIndex--;
             }
-            else {
-                valid = 0;
-                break;
+            else
+            {
+                printf("Found an unmatched parentheses\n");
+                return 0;
             }
         }
-         */
-
-        if (seq[i] == '(' || seq[i] == '[' || seq[i] == '{') {
-            openParentheses[i] = seq[i];
-        }
-        //printf(openParentheses);
-        //printf("\n");
-
-        for (int i = 0; i < strlen(seq); i++) {
-            if (seq[i] == ')') {
-                if (openParentheses[i-1] != '('){
-                    valid = 0;
-                    break;
-                }
+        else if (input[i] == ']') //case for other close parentheses
+        {
+            if (stack[stackIndex - 1] == '[')
+            {
+                stackIndex--;
             }
-            if (seq[i] == ']') {
-                if (openParentheses[i-1] != '['){
-                    valid = 0;
-                    break;
-                }
-            }
-            if (seq[i] == '}') {
-                if (openParentheses[i-1] != '{'){
-                    valid = 0;
-                    break;
-                }
+            else
+            {
+                printf("Found an unmatched parentheses\n");
+                return 0;
             }
         }
     }
-    return valid;
+    printf("The sequence is balanced\n"); //prints the result if all are matched
+    return 1; //indicates true value
 }
 
 int main(int argc, char *argv []) {
 
     FILE *fp;
-    int numOfExpressions = 0;
+    int numOfExpressions; //stores number of lines to validate
     char *filename;
-    char fileData[50];
     filename = (char *) malloc(sizeof(char) * 15);
-
-    strcpy(filename, argv[1]);
-
-    printf("The file name is: %s\n", filename);
-
+    strcpy(filename, argv[1]); //takes argument file to run
     fp = fopen(filename, "r");
-
     fscanf(fp, "%d", &numOfExpressions);
-    printf("There are %d lines in this document: \n", numOfExpressions);
+    printf("\nThere are %d lines in this document: \n\n", numOfExpressions);
 
-    if (fp != NULL) {
-
-        for (int i = 0; i < numOfExpressions; i++) {
-            fscanf(fp, "%s", sequence);
-            printf("%s", sequence);
-            validateSequence(sequence);
-            if (valid) {
+    if (fp != NULL)
+    {
+        for (int i = 0; i < numOfExpressions; i++)
+        {
+            fscanf(fp, "%s", sequence); //scans the sequence line from file
+            printf("%s - ", sequence); //prints the sequence
+            valid = checkExpression(sequence); //runs the sequence through the validating function
+            if (valid) //prints more info indicating if sequence is valid
+            {
                 printf(" - correct\n");
-            } else {
-                printf(" - incorrect\n");
             }
+            else
+                {
+                printf(" - incorrect\n");
+                }
         }
-        /*
-        while (fgets(fileData, 35, fp) != NULL) {
-            printf("%s", fileData);
-        }
-         */
-
         printf("\n");
-
     }
 
-
-
-    printf("File closed.\n");
-    fclose(fp);
+    fclose(fp); //close the input file
     return 0;
 }
